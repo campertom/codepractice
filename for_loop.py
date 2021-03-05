0c77dbb8634c66e44bbc3be51ff1c95376873062
Tommy D <campertom@yahoo.com>
2021-03-04 20:25:36 -0500

    random but friendly for loop with Leslie

--- for_loop.py
+++ for_loop.py
@@ -1,4 +1,5 @@
 # goal: create for loop that manipulates an array, or list of strings
+import numpy  npas
 firstArray = [1,6,7,3,6,9,0]
 secondArray = [5,3,0,3,2,8,2]
 
---
+++ square_math.py
@@ -0,0 +1,10 @@
+"""
+num = 1
+while ( num <= 10):
+    print(num, "\t\t", num * num)
+    num+=1
+"""
+for i in range (1, 9):
+    #print("\t\t", num*num)
+    print(i)
+    
