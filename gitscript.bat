407d7e1ae23543d692e1f44457c09b4da5b256c5
Thomas DiBenedetto <campertom550@gmail.com>
2021-03-19 19:58:31 -0400

    are we there yet?

--- gitscript.bat
+++ gitscript.bat
@@ -1,19 +1,16 @@
-@echo off
-ECHO SOURCE BRANCH IS %BUILD_SOURCEBRANCH%
-IF %BUILD_SOURCEBRANCH% == refs/heads/main (
-   ECHO Building main branch so no merge is needed.
-   EXIT
-)
-SET sourceBranch=origin/%BUILD_SOURCEBRANCH:refs/heads/=%
+ECHO SOURCE BRANCH IS %NerveBranch%
+
 ECHO GIT CHECKOUT MAIN
 git checkout main
 ECHO GIT STATUS
 git status
 ECHO GIT MERGE
-git merge %sourceBranch% -m "Merge to main"
+git merge %NerveBranch% -m "Merge to main"
 ECHO GIT STATUS
 git status
 ECHO GIT PUSH
 git push origin
 ECHO GIT STATUS
 git status
+
+
