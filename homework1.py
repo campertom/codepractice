2c8488f3cd29d578a4072d43a153e85d6faf6f60
Tommy D <campertom@yahoo.com>
2021-09-22 06:53:04 -0400

    homework night 2

--- homework1.py
+++ homework1.py
@@ -1,2 +1,10 @@
-name = "ada lovelace" 
-print(name.title())
+first_name = "ada" 
+last_name = "lovelace" 
+full_name = f"{first_name} {last_name}" 
+message = f"Hello, {full_name.title()}!" 
+print(message)
+
+first_name = "ada" 
+last_name = "lovelace" 
+full_name = f"{first_name} {last_name}" 
+print(full_name) 
