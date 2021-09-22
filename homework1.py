4663ea0c5088604a9d764ca4aa976a81b5c2207e
Tommy D <campertom@yahoo.com>
2021-09-22 16:56:33 -0400

    transfering file between systems in office

--- homework1.py
+++ homework1.py
@@ -3,8 +3,20 @@ last_name = "lovelace"
 full_name = f"{first_name} {last_name}" 
 message = f"Hello, {full_name.title()}!" 
 print(message)
-
+#f-string below: makes the interpreter call the value of the variable
 first_name = "ada" 
 last_name = "lovelace" 
 full_name = f"{first_name} {last_name}" 
 print(full_name) 
+# tab and new line
+print("Python")
+print("\tPython")
+
+print("Languages:\n\tPython\nC\n\tJavaScript\nRuby\nGO") 
+# #whoa bitch that was crazy
+favorite_language = 'python '
+favorite_language
+favorite_language.rstrip()
+
+new_language = ' python'
+new_language.lstrip()
